6826fc800507c9d7b7fa99cb0d0759ed65c60927
Petros Kyres <59505322+petrandent@users.noreply.github.com>
2021-08-22 16:37:52 +0300

    Update skew.py

--- skew.py
+++ skew.py
@@ -15,14 +15,40 @@ for i in genome_text:
     elif i=="C":
         y-=1
     else:
-        continue
+        y=y
     skew_array.append(y)
 
 x_axis=list(range(len(skew_array)))
 
-print(len(x_axis))
-plt.plot(x_axis,skew_array)
-plt.show()
+minimum_skew=min(skew_array)
+minimum_position=skew_array.index(minimum_skew)
+print(minimum_position)
 
 
 
+#skey plot
+#plt.xlabel("position")
+#plt.ylabel("skew")
+#plt.title("Skew diagram")
+#plt.plot(x_axis,skew_array)
+#plt.show()
+
+def most_frequent_kmer(text,k=9):
+    kmers={}
+    for i in range(len(text)-k+1):
+        subpattern=text[i:(i+k)]
+        if subpattern in kmers:
+            kmers[subpattern]+=1
+        else:
+            kmers[subpattern]=1
+    maximum_value=max(kmers.values())
+    frequent_kmers=[]
+    for key,v in kmers.items():
+        if v==maximum_value:
+            frequent_kmers.append(key)
+        else:continue
+    return frequent_kmers
+
+#testing
+a=most_frequent_kmer("aaaattattatattttttaaaaa",2)
+print(a)
